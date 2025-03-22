c35cd662310ef19dbcfeeb6cee165c35137e1453
Tong Long Dev <tonglong.dev@gmail.com>
2025-03-22 15:52:16 +0700

    2677.chunkArray

---
+++ chunkArray.js
@@ -0,0 +1,8 @@
+function chunkArray(arr, size) {
+  const result = [];
+  for (let i = 0; i < arr.length; i += size) {
+    result.push(arr.slice(i, i + size));
+  }
+  return result;
+}
+console.log(chunkArray([1, 2, 3, 4, 5], 3));
