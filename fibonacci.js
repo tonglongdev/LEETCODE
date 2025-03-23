b1c41033a8328f5800500f7eca2c3e33e993c63b
Tong Long Dev <tonglong.dev@gmail.com>
2025-03-23 17:39:18 +0700

    2648.fibonacciSequence

--- fibonacci.js
+++ fibonacci.js
@@ -3,4 +3,15 @@ function fibonacci(n) {
   return fibonacci(n - 1) + fibonacci(n - 2);
 }
 // 1,1,2,3,5,8
-console.log(fibonacci(6))
+// console.log(fibonacci(6))
+
+function* fibGenerator() {
+  let a = 0,
+    b = 1;
+  while (true) {
+    yield a;
+    [a, b] = [b, a + b];
+  }
+}
+const gen = fibGenerator();
+console.log(gen.next().value);
