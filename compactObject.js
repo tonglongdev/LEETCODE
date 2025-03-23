5a19d207e79c44bc2b57acc46ff1b8c32de0715b
Tong Long Dev <tonglong.dev@gmail.com>
2025-03-23 11:50:22 +0700

    2705.compactObject

---
+++ compactObject.js
@@ -0,0 +1,25 @@
+function compactObject(obj) {
+  if (Array.isArray(obj)) {
+    return obj.filter(Boolean).map(compactObject);
+  } else if (typeof obj === "object" && obj !== "null") {
+    const result = {};
+    for (let key in obj) {
+      if (Boolean(obj[key])) {
+        result[key] = compactObject(obj[key]);
+      }
+    }
+    return result;
+  } else {
+    return obj;
+  }
+}
+
+const arrExp = [[null, 0, 5, [0], [false, 16]]];
+
+const objExp = {
+  user: { name: "Alice", age: 0, email: null },
+  active: false,
+  roles: ["admin", "", null],
+};
+
+console.log(compactObject(objExp));
