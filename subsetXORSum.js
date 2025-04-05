1fd5742dabdd8458bfd6a8065e630f2b829f43b3
Tong Long Dev <tonglongdev@192.168.1.4>
2025-04-05 17:07:50 +0700

    1863. Sum of All Subset XOR Totals

--- subsetXORSum.js
+++ subsetXORSum.js
@@ -15,11 +15,30 @@
 //     return total;
 // };
 
+// =======================
+// Binary tree
+// dfs(0, 0)
+// /         \
+// ❌ bỏ 1       ✅ chọn 1
+// /                 \
+// dfs(1,0)         dfs(1, 1)
+// /   \            /    \
+// ❌2  ✅2        ❌2    ✅2
+// (2,0)(2,2)     (2,1)   (2,3)
+
+// dfs(2, 0) → total += 0
+// dfs(2, 3) → total += 3
+// dfs(2, 1) → total += 1
+// dfs(2, 2) → total += 2
+// Tổng cuối cùng: 0 + 3 + 1 + 2 = 6
+// =======================
+
 //Phương pháp 2: Sử Dụng Tính Chất Bitwise OR
 var subsetXORSum = function (nums) {
-    let bitOr = 0;
-    for (let num of nums) {
-        bitOr |= num;
-    }
-    return bitOr * (1 << (nums.length - 1));
+  let bitOr = 0;
+  for (let num of nums) {
+    bitOr |= num;
+  }
+  // (1 << (nums.length - 1)) => 2^(n - 1)
+  return bitOr * (1 << (nums.length - 1));
 };
