7f1b1928c87660f12aded69b5613ddd1fefbf7a6
TongLongDev <tonglong.dev@gmail.com>
2026-07-21 11:18:29 +0700

    3499. Maximize Active Section with Trade I

---
+++ maximize-active-section-with-trade-i.py
@@ -0,0 +1,25 @@
+class Solution:
+    def maxActiveSectionsAfterTrade(self, s: str) -> int:
+        n = len(s)
+        cnt1 = s.count("1")
+
+        zeroBlocks = []
+        i = 0
+        while i < n:
+            start = i
+
+            while i < n and s[i] == s[start]:
+                i += 1
+
+            if s[start] == "0":
+                zeroBlocks.append(i - start)
+
+        m = len(zeroBlocks)
+
+        if m < 2:
+            return cnt1
+
+        bestGain = 0  # Optimal Increment
+        for i in range(m - 1):
+            bestGain = max(bestGain, zeroBlocks[i] + zeroBlocks[i + 1])
+        return cnt1 + bestGain
